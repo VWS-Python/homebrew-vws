82bfcd8cd33d820db1915549a71ca80abbbff598
Adam Dangoor <adamdangoor@gmail.com>
2021-12-13 05:25:06 +0000

    Homebrew recipe for version 2021.12.13.15

--- vws-cli.rb
+++ vws-cli.rb
@@ -1,7 +1,7 @@
 class VwsCli < Formula
   include Language::Python::Virtualenv
 
-  url "https://codeload.github.com/VWS-Python/vws-cli/legacy.tar.gz/2021.12.13.14"
+  url "https://codeload.github.com/VWS-Python/vws-cli/legacy.tar.gz/2021.12.13.15"
   head "https://github.com/VWS-Python/vws-cli.git"
   homepage ""
   depends_on "python@3.9"
