1dafb9cd436431c358fb2f56b7815279203340d9
adamtheturtle <adamdangoor@gmail.com>
2024-09-11 22:56:43 +0000

    Bump VWS CLI Homebrew recipe

--- vws-cli.rb
+++ vws-cli.rb
@@ -2,15 +2,20 @@ class VwsCli < Formula
   include Language::Python::Virtualenv
 
   desc "CLI for Vuforia Web Services"
-  homepage ""
-  url "https://files.pythonhosted.org/packages/92/7b/e0df4d8eb621cbbad60736b1b6c2d76bb64027affed375fbb94007b193de/vws-cli-2024.2.21.4.tar.gz"
-  sha256 "3a411ade3bd6930485f00d9503c11c29b5598dd19265bef89847377bbe3e8c7f"
+  homepage "None"
+  url "https://files.pythonhosted.org/packages/42/97/4dd446708cc1761807132d928cdcb6231a680c09de91a322d2ae2b52a156/vws_cli-2024.9.11.tar.gz"
+  sha256 "b2f458847e3d52ad0f4296cf30b1fa76d905af2d3c064cf65c5eb4ce4ac0c584"
 
   depends_on "python3"
 
+  resource "beartype" do
+    url "https://files.pythonhosted.org/packages/96/15/4e623478a9628ad4cee2391f19aba0b16c1dd6fedcb2a399f0928097b597/beartype-0.18.5.tar.gz"
+    sha256 "264ddc2f1da9ec94ff639141fbe33d22e12a9f75aa863b83b7046ffff1381927"
+  end
+
   resource "certifi" do
-    url "https://files.pythonhosted.org/packages/71/da/e94e26401b62acd6d91df2b52954aceb7f561743aa5ccc32152886c76c96/certifi-2024.2.2.tar.gz"
-    sha256 "0569859f95fc761b18b45ef421b1290a0f65f147e92a1e5eb3e635f9a5e4e66f"
+    url "https://files.pythonhosted.org/packages/b0/ee/9b19140fe824b367c04c5e1b369942dd754c4c5462d5674002f75c4dedc1/certifi-2024.8.30.tar.gz"
+    sha256 "bec941d2aa8195e248a60b31ff9f0558284cf01a52591ceda73ea9afffd69fd9"
   end
 
   resource "charset-normalizer" do
@@ -24,33 +29,33 @@ class VwsCli < Formula
   end
 
   resource "idna" do
-    url "https://files.pythonhosted.org/packages/bf/3f/ea4b9117521a1e9c50344b909be7886dd00a519552724809bb1f486986c2/idna-3.6.tar.gz"
-    sha256 "9ecdbbd083b06798ae1e86adcbfe8ab1479cf864e4ee30fe4e46a003d12491ca"
+    url "https://files.pythonhosted.org/packages/e8/ac/e349c5e6d4543326c6883ee9491e3921e0d07b55fdf3cce184b40d63e72a/idna-3.8.tar.gz"
+    sha256 "d838c2c0ed6fced7693d5e8ab8e734d5f8fda53a039c0164afb0b82e771e3603"
   end
 
   resource "PyYAML" do
-    url "https://files.pythonhosted.org/packages/cd/e5/af35f7ea75cf72f2cd079c95ee16797de7cd71f29ea7c68ae5ce7be1eda0/PyYAML-6.0.1.tar.gz"
-    sha256 "bfdf460b1736c775f2ba9f6a92bca30bc2095067b8a9d77876d1fad6cc3b4a43"
+    url "https://files.pythonhosted.org/packages/54/ed/79a089b6be93607fa5cdaedf301d7dfb23af5f25c398d5ead2525b063e17/pyyaml-6.0.2.tar.gz"
+    sha256 "d584d9ec91ad65861cc08d42e834324ef890a082e591037abe114850ff7bbc3e"
   end
 
   resource "requests" do
-    url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
-    sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
+    url "https://files.pythonhosted.org/packages/63/70/2bf7780ad2d390a8d301ad0b550f1581eadbd9a20f896afe06353c2a2913/requests-2.32.3.tar.gz"
+    sha256 "55365417734eb18255590a9ff9eb97e9e1da868d4ccd6402399eaf68af20a760"
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/7a/50/7fd50a27caa0652cd4caf224aa87741ea41d3265ad13f010886167cfcc79/urllib3-2.2.1.tar.gz"
-    sha256 "d0570876c61ab9e520d776c38acbbb5b05a776d3f9ff98a5c8fd5162a444cf19"
+    url "https://files.pythonhosted.org/packages/43/6d/fa469ae21497ddc8bc93e5877702dca7cb8f911e337aca7452b5724f1bb6/urllib3-2.2.2.tar.gz"
+    sha256 "dd505485549a7a552833da5e6063639d0d177c04f23bc3864e41e5dc5f612168"
   end
 
   resource "vws-auth-tools" do
-    url "https://files.pythonhosted.org/packages/21/9e/ff3ab55740d31fc6fcc63e5eb96eabad48c86f84dda5504a0385325554c4/vws-auth-tools-2023.3.5.tar.gz"
-    sha256 "4b29464aac88c0cce4e7e696c10cbdb5735545565c1d8ae5f2dd5db24f21e6f6"
+    url "https://files.pythonhosted.org/packages/a9/17/421ff3a46cee7d952e3da3126160fb75ab7cb54e3fbbfa718a7ee9e120d4/vws_auth_tools-2024.7.12.tar.gz"
+    sha256 "e3949606f2366053ea97883992f8ecaf95030ea33f1b3cf769f99f9d43c0914b"
   end
 
   resource "vws-python" do
-    url "https://files.pythonhosted.org/packages/d2/cd/9e0fef8be88a6e6f79c402fad08c4b42797652ab451a4ed31e0d46ee43b2/vws-python-2024.2.19.tar.gz"
-    sha256 "cc765b30de513dc84c6ff030ee8cc3cc8d50bc5f8f9e5b7a0780eb5b20a986a3"
+    url "https://files.pythonhosted.org/packages/00/29/b2a2d75cb4ab192b2dbd974b4fbd6dfd95eccbbac58704d00b72e8870630/vws_python-2024.9.4.1.tar.gz"
+    sha256 "877cef5c0122f30ce42f7ea48116c943899570b6300f790ef9ead66d870b329d"
   end
 
   def install
